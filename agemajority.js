ce284018cbf276eb1319402800d2811009523917
Isaac Samuel <142616848+Isaac0912@users.noreply.github.com>
2026-05-10 13:59:24 -0300

    Delete agemajority.js
    Organizing javascript files

--- agemajority.js
+++
@@ -1,9 +0,0 @@
-let age = Number(prompt("What is your age?"));
-
-if (age < 0) {
-    console.log("Invalid input.");
-} else if (age < 18) {
-    console.log("You are a minor.");
-} else {
-    console.log("You have legal age.")
-}
